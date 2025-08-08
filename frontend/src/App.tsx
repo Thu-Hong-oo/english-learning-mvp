f8d98592786691328f4f2db3fab4d73ba91418c2
Thu-Hong-oo <thuhong05022003@gmail.com>
2025-08-08 12:31:27 +0700

    feature(ui): Add featurecourse

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/layout/Header'
 import HeroSection from './components/layout/HeroSection'
 import TopCategories from './components/layout/Top-Category'
 import StudentFeedback from './components/layout/StudentFeedback'
+import FeaturedCourses from './components/layout/FeatureCourse'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <HeroSection />
       <TopCategories />
+      <FeaturedCourses/>
       <StudentFeedback />
     </>
   )
---
+++ frontend/src/components/layout/LearnPressAddOns.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LearnPressAddOns = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LearnPressAddOns
