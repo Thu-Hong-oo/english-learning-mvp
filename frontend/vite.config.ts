00361a507a02cfbf5710cfb0c5d06409f90bb972
Thu-Hong-oo <thuhong05022003@gmail.com>
2025-08-15 10:22:05 +0700

    fix deploy: config vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -30,4 +31,4 @@ export default defineConfig({
     port: 4173,
     host: true,
   },
-})
+});
